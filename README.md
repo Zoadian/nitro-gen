79b1026c515fc38ebd6841bd5cadd2d423ad1c59
Zoadian <github@zoadian.de>
2014-02-13 21:08:03 +0100

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# nitro-gen
+
+Nitro is an Entity Component System (ECS) for the D Programming Language.
---
+++ source/nitro/gen/package.d
@@ -0,0 +1,11 @@
+﻿/***********************************************************************************************************************
+Nitro.gen: Helpers to automatically generate Nitro
+
+Copyright: Copyright Felix 'Zoadian' Hufnagel 2014- and Paul Freund 2014-.
+License: a$(WEB boost.org/LICENSE_1_0.txt, Boost License 1.0).
+Authors: $(WEB zoadian.de, Felix 'Zoadian' Hufnagel) and $(WEB lvl3.org, Paul Freund).
+*/
+module nitro.gen;
+
+public import nitro.gen.ecs;
+public import nitro.gen.query;
